19816ac88f3d50a21a455e631764982573ba4382
CrisSantos91 <cris91@hotmail.com.br>
2017-11-16 18:14:30 -0200

    projeto loja

---
+++ adiciona-produto.php
@@ -0,0 +1,28 @@
+<?php include("cabecalho.php"); ?>
+
+<?php
+$nome = $_GET["nome"];
+$preco = $_GET["preco"];
+$conexao = mysqli_connect('localhost', 'root', '','loja');
+
+$query = "INSERT into produtos (nome, preco)VALUES ( '{$nome}', {$preco})";
+?>
+<?php
+if (mysqli_query($conexao, $query)){
+	?>
+	<p class="alert-success">
+		Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!
+	</p>
+	<?php
+}else {
+	?>
+
+	<p class="alert-danger">
+		Produto <?= $nome; ?> não foi adicionado!</p>
+		<?php
+
+	}
+	?>
+
+
+	<?php include("rodape.php"); ?>
