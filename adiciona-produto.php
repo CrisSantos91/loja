e255c228b2efc9f82c3ff1161986d4ee95a9031b
CrisSantos91 <cris91@hotmail.com.br>
2017-11-17 16:51:35 -0200

    criado sobre da loja e comandos php

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,32 +1,24 @@
 <?php include("cabecalho.php"); ?>
+
 <?php
-function insereProduto($conexao, $nome, $preco){
-	$query = "INSERT into produtos (nome, preco)VALUES ( '{$nome}', {$preco})";
-	$resultadoDaInsercao = mysqli_query($conexao, $query); 
-	return $resultadoDaInsercao;
+
+function insereProduto($conexao, $nome, $preco) {
+    $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+    return mysqli_query($conexao, $query);
 }
 
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
-$conexao = mysqli_connect('localhost', 'root', '','loja');
+$conexao = mysqli_connect('localhost', 'root', '', 'loja');
 
+if(insereProduto($conexao, $nome, $preco)) { ?>
+    <p class="text-success">O produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+<?php } else {
+    $msg = mysqli_error($conexao);
+?>
+    <p class="text-danger">O produto <?= $nome; ?> não foi adicionado: <?= $msg ?></p>
+<?php
+}
+?>
 
-
-if (insereProduto($conexao, $nome, $preco)){
-	?>
-	<p class="text-success">
-		Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!
-	</p>
-	<?php
-}else {
-	$msg = mysqli_error($conexao);
-	?>
-
-	<p class="text-danger">
-		Produto <?= $nome; ?> não foi adicionado: <?=$msg?></p>
-		<?php
-	}
-	?>
-
-
-	<?php include("rodape.php"); ?>
+<?php include("rodape.php"); ?>
--- produto-formulario.php
+++ produto-formulario.php
@@ -2,29 +2,19 @@
 
 <h1>Formulário de cadastro</h1>
 <form action="adiciona-produto.php">
-
-	<table class="table">
-
-		<tr>
-			<td>Nome:</td>
-			<td><input class="form-control" type="text" name="nome" /><br/></td>
-
-		</tr>
-
-		<tr>
-			<td>Preço:</td>
-			<td><input class="form-control" type="number" name="preco" /><br/></td>
-
-		</tr>
-		<tr>
-			<td><button class="btn btn-primary" type="submit">Cadastrar</button>
-			</td>
-
-		</tr>
-
-	</table>
-
-
+    <table class="table">
+        <tr>
+            <td>Nome</td>
+            <td><input class="form-control" type="text" name="nome" /></td>
+        </tr>
+        <tr>
+            <td>Preço</td>
+            <td><input class="form-control" type="number" name="preco" /></td>
+        </tr>
+        <tr>
+            <td><button class="btn btn-primary" type="submit">Cadastrar</button></td>
+        </tr>
+    </table>
 </form>
 
 <?php include("rodape.php"); ?>
