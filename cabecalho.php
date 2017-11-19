f78ef7f81f123eeff1704e2cc868022a8c1b34ed
CrisSantos91 <cris91@hotmail.com.br>
2017-11-19 12:18:31 -0200

    adiciona categoria do type radio

--- cabecalho.php
+++ cabecalho.php
@@ -7,20 +7,22 @@
 </head>
 <body>
     <div class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <a href="index.php" class="navbar-brand">Minha Loja</a>
-            </div>
-            <div>
-                <ul class="nav navbar-nav">
-                    <li><a href="produto-formulario.php">Adiciona Produto</a></li>
-                    <li><a href="produto-lista.php">Produtos</a></li>
-                    <li><a href="sobre.php">Sobre</a></li>
-                </ul>
-            </div>
+        <div class="container-fluid">
+
+            <table class="table table-bordered table-hover table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col"><a href="index.php" class="navbar-brand">Minha Loja</a></th>
+                        <th scope="col" ><a href="produto-formulario.php">Adiciona Produto</a></th>
+                        <th scope="col"><a href="produto-lista.php">Produtos</a></th>
+                        <th scope="col"><a href="sobre.php">Sobre</a></th>
+                    </tr>
+                </thead>
+            </table>
+            
         </div>
     </div>
 
     <div class="container">
 
-    <div class="principal">
+        <div class="principal">
